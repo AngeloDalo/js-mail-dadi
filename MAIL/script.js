3e7995b19f8b00af163f1388fe2fd5a810c960e0
AngeloDalo <angelo251158@gmail.com>
2021-11-24 14:13:27 +0100

    controllo

--- MAIL/script.js
+++ MAIL/script.js
@@ -7,18 +7,21 @@
 const emailList = ["email1@gmail.com", "email2@gmail.com", "email3@gmail.com", "email4@gmail.com", "email4@gmail.com"];
 const buttonControl = document.querySelector('.button');
 const emailUser = document.getElementById("email")
+const answersCheck = document.getElementById('answer');
 let check = 0;
 
 buttonControl.addEventListener ( 'click', function() {
    for (i=0; i<=emailList.length; ++i) {
-       if (emailUser.value == emailList[i]) {
+       if (emailUser.value.toLowerCase() == emailList[i]) {
            check = 1;
        }
     }
     
     if (check==1) {
         console.log("email presente nel database");
+        answersCheck.innerHTML = "email " + emailUser.value.toLowerCase() + " presente nel database";
     } else {
         console.log("email non presente nel database");
+        answersCheck.innerHTML = "email " + emailUser.value.toLowerCase() + " non presente nel database";
     }
 });
