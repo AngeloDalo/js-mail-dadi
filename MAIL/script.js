2fe3c3c14f33942075f3d62cf5842b8c11998768
AngeloDalo <angelo251158@gmail.com>
2021-11-24 13:45:54 +0100

    html

---
+++ MAIL/script.js
@@ -0,0 +1,7 @@
+/**
+ * chiedi email all'utente
+ * controlla che sia nella lista di chi può accedere
+ * stampa un messaggio appropriato sull'esito del controllo
+ */
+
+const emailList = ["email1.gmail.com", "email2.gmail.com", "email3.gmail.com", "email4.gmail.com", "email4.gmail.com"];
