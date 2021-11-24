16a4bc705ab788b90eb0d4a15278f4849a34a387
AngeloDalo <angelo251158@gmail.com>
2021-11-24 13:57:38 +0100

    base

--- MAIL/script.js
+++ MAIL/script.js
@@ -4,4 +4,21 @@
  * stampa un messaggio appropriato sull'esito del controllo
  */
 
-const emailList = ["email1.gmail.com", "email2.gmail.com", "email3.gmail.com", "email4.gmail.com", "email4.gmail.com"];
+const emailList = ["email1@gmail.com", "email2@gmail.com", "email3@gmail.com", "email4@gmail.com", "email4@gmail.com"];
+const buttonControl = document.querySelector('.button');
+const emailUser = document.getElementById("email")
+let check = 0;
+
+buttonControl.addEventListener ( 'click', function() {
+   for (i=0; i<=emailList.length; ++i) {
+       if (emailUser.value == emailList[i]) {
+           check = 1;
+       }
+    }
+    
+    if (check==1) {
+        console.log("email presente nel database");
+    } else {
+        console.log("email non presente nel database");
+    }
+});
